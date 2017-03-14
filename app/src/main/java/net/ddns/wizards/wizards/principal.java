19f60b05ee44168ab7a6c99458a0af0fa33dbaff
Miguel Pons <nickpons666@hotmail.com>
2017-03-14 16:41:57 -0600

    Login Ok

--- app/src/main/java/net/ddns/wizards/wizards/principal.java
+++ app/src/main/java/net/ddns/wizards/wizards/principal.java
@@ -4,11 +4,11 @@ import android.content.Intent;
 import android.net.http.LoggingEventHandler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
 
 public class principal extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
