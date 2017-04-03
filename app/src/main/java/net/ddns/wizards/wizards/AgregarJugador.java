765ec5dc80f0877164e929512a2e9f43b5ffa1f2
nickpons666 <miguel.pons@outlook.com>
2017-04-03 10:23:51 -0500

    MenúAdministrador

--- app/src/main/java/net/ddns/wizards/wizards/AgregarJugador.java
+++ app/src/main/java/net/ddns/wizards/wizards/AgregarJugador.java
@@ -5,15 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MenuAdministrador extends AppCompatActivity {
+public class AgregarJugador extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu_administrador);
+        setContentView(R.layout.activity_agregar_jugador);
     }
-    public void Agregarjugador (View view){
-        Intent i = new Intent(this, AgregarJugador.class);
+
+    public void menuAdministrador (View view){
+        Intent i = new Intent(this, MenuAdministrador.class);
         startActivity(i);
         finish();
     }
